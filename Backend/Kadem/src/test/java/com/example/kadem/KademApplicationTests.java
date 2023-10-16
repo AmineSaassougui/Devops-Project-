d84a1e6959ed967cec4cc89f83bb75bca7a0f6dd
Amineesprit007 <mohamedamine.saassougui@esprit.tn>
2023-10-16 16:01:52 +0100

    first

---
+++ Backend/Kadem/src/test/java/com/example/kadem/KademApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kadem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KademApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
