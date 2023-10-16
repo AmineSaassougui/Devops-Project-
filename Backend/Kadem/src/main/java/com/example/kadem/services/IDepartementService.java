ab21fec6c773e320dc0675ebe716bfb06007eba4
Achref Zitoun <achref.zitoun@esprit.tn>
2023-10-16 17:15:03 +0100

    class departement

---
+++ Backend/Kadem/src/main/java/com/example/kadem/services/IDepartementService.java
@@ -0,0 +1,13 @@
+package com.example.kadem.services;
+
+import com.example.kadem.entities.Departement;
+
+import java.util.List;
+
+public interface IDepartementService {
+    List<Departement> retrieveAllDepartements();
+    Departement addDepartement(Departement d);
+    Departement updateDepartement(Departement d);
+    Departement retrieveDepartement(Integer idDepart);
+    public List<Departement> retrieveDepartementsByUniversite(Integer idUniversite);
+}
