02a8dbc5f08c0682cf47cb1ccef507fd764c61ef
Cherif <cherif.mohamedamine@esprit.tn>
2023-10-19 22:25:18 +0100

    add test mockito

--- Backend/Kadem/src/main/java/com/example/kadem/repositories/EtudiantRepository.java
+++ Backend/Kadem/src/main/java/com/example/kadem/repositories/EtudiantRepository.java
@@ -3,6 +3,7 @@ import com.example.kadem.entities.Etudiant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 
 
 @Repository
---
+++ Backend/Kadem/src/test/java/com/example/kadem/EquipeRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.example.kadem;
+
+import com.example.kadem.entities.Equipe;
+import com.example.kadem.entities.Niveau;
+import com.example.kadem.repositories.EquipeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+@Slf4j
+@ExtendWith(SpringExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class EquipeRepositoryTest {
+    @Mock
+    EquipeRepository equipeRepository;
+
+    static Equipe e = Equipe.builder().nomEquipe("Equipe7").niveau(Niveau.EXPERT).build();
+    List<Equipe> list = new ArrayList<Equipe>() ;
+
+
+
+}
