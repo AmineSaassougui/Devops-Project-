61a701df14cd7ba582ad4603ad2fb0eee3f2cf41
Achref Zitoun <92675276+achrefzitoun@users.noreply.github.com>
2023-10-19 21:44:59 +0100

    Update EtudiantRepository.java

--- Backend/Kadem/src/main/java/com/example/kadem/repositories/EtudiantRepository.java
+++ Backend/Kadem/src/main/java/com/example/kadem/repositories/EtudiantRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface EtudiantRepository extends JpaRepository<Etudiant, Integer> {
 
-    List<Etudiant> findByDepartementIdDepartement(Integer idDepartement);
+    // List<Etudiant> findByDepartementIdDepartement(Integer idDepartement);
 
 
 
